f99981b02dbb6c71bfc1c2e558d0b9fba78924cf
Nguyen Huu Nghia <luffy.1999tm@gmail.com>
2022-06-23 10:46:56 +0700

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pawpubbleclone, PawpubblecloneWeb.Endpoint,
-  url: [scheme: "https", host: "rocky-stream-82118.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "pawpubble.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 # Do not print debug messages in production
